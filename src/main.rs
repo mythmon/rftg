55c34ccc2ae828876df3c3a9d62999df21a8e8c6
Mike Cooper <mythmon@gmail.com>
2015-03-16 00:06:14 -0700

    Add pretty print to cards.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![feature(core)]
 #![feature(collections)]
 #![feature(io)]
-#![feature(box_syntax)]
 
 extern crate rand;
 
@@ -102,13 +101,13 @@ impl<'a> PlayArea<'a> {
 
         println!("Your tableau:");
         for card in self.tableau.iter() {
-            println!("\t{:?}", card);
+            println!("    {}", card);
         }
         println!("");
 
         println!("Your hand:");
         for card in self.hand.iter() {
-            println!("\t{:?}", card);
+            println!("    {}", card);
         }
         println!("");
 
@@ -130,7 +129,7 @@ impl<'a> PlayArea<'a> {
 
         println!("Exploring");
         for (i, card) in explore_cards.iter().enumerate() {
-            println!("\t{}) {:?}", i + 1, card);
+            println!("    {}) {}", i + 1, card);
         }
         println!("");
 
@@ -165,7 +164,7 @@ impl<'a> PlayArea<'a> {
 
         println!("Your hand:");
         for card in self.hand.iter() {
-            println!("\t{:?}", card);
+            println!("    {}", card);
         }
     }
 }
@@ -293,6 +292,54 @@ impl Card {
     }
 }
 
+impl fmt::Display for Card {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let mut parts: Vec<String> = vec![self.name.clone()];
+
+        parts.push(match self.card_type {
+            CardType::World => {
+                if self.military_cost > 0 {
+                    assert!(self.trade_cost == 0);
+                    format!("({:?} - {} military)", self.card_type, self.military_cost)
+                } else if self.trade_cost > 0 {
+                    format!("({:?} - {} trade)", self.card_type, self.trade_cost)
+                } else {
+                    format!("({:?} - free)", self.card_type)
+                }
+            },
+            CardType::Development => {
+                if self.military_cost > 0 {
+                    assert!(self.trade_cost == 0);
+                    format!("<{:?} - {} military>", self.card_type, self.military_cost)
+                } else if self.trade_cost > 0 {
+                    format!("<{:?} - {} trade>", self.card_type, self.trade_cost)
+                } else {
+                    format!("<{:?} - free>", self.card_type)
+                }
+            },
+        });
+
+        if self.victory_points > 0 {
+            parts.push(format!("{{{} VPs}}", self.victory_points));
+        }
+
+        match (&self.production, &self.good) {
+            (&Some(ref prod), &Some(ref good)) => {
+                parts.push(format!("{:?}: {:?}", prod, good));
+            },
+            (&None, &None) => {},
+            _ => panic!("Inconsistent good/production values!"),
+        }
+
+        for power in self.powers.iter() {
+            parts.push(format!("{:?}", power));
+        }
+
+        fmt.write_str(parts.connect(" ").as_slice()).ok().expect("Could not format string.");
+        Result::Ok(())
+    }
+}
+
 fn get_cards() -> Vec<Card> {
     vec![
         Card::new("Alien Robot Sentry")
@@ -348,6 +395,25 @@ fn get_cards() -> Vec<Card> {
             .good(Good::RareElements)
             .production(Production::Windfall),
 
+        Card::new("Galactic Renaissance")
+            .card_type(CardType::Development)
+            .trade_cost(6)
+            .add_power(Power::ExploreSeeBonus(2))
+            .add_power(Power::ExploreKeepBonus(1)),
+            // Special scoring
+            //   * +1VP for every VP in chips, divided by 3 rounded down
+            //   * +3VP for cards: name="Research Labs"
+            //   * +3VP for cards: name="Galactic Trendsetters"
+            //   * +3VP for cards: name="Artist Colony"
+
+        Card::new("Galactic Survey: SETI")
+            .card_type(CardType::Development)
+            .trade_cost(6)
+            .add_power(Power::ExploreSeeBonus(2)),
+            // Special scoring
+            //   * +1VP for cards: has_phase=1
+            //   * +1VP for cards: card_type=World
+
         Card::new("The Last of the  Uplift Gnarssh")
             .card_type(CardType::World)
             .military_cost(1)
@@ -389,12 +455,6 @@ fn get_cards() -> Vec<Card> {
             .victory_points(2)
             .good(Good::Genes)
             .production(Production::Windfall),
-
-        Card::new("Galactic Renaissance")
-            .card_type(CardType::Development)
-            .trade_cost(6)
-            .add_power(Power::ExploreSeeBonus(2))
-            .add_power(Power::ExploreKeepBonus(1))
     ]
 }
 
